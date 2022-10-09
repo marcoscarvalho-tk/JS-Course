5811fe6e0145c8b954d1a8eaf8e7c0146e361eff
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 <marcoscarvalho.com@outlook.com>
2022-10-08 22:26:17 -0300

    insert new aula13/ambiente.js file

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+Dica14
+//estrutura de repetição lógica com teste no inicio
+var c = 1
+while ( c  < 6){
+    console.log (`Passo ${c}?`)
+    c++
+}
+
+//estrutura de repetição lógica com teste no final
+
+var c = 1
+do{
+    console.log (`Passo ${c}?`)
+    c++
+}while ( c  < 6)

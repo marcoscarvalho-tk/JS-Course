5859754af6b284f33f14f14b86d42e01d3d9b4f0
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 <marcoscarvalho.com@outlook.com>
2022-10-15 23:49:47 -0300

    insert new aula16/funcao02.js file

---
+++ aula16/funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0){    /* se o n1 ou n2 nao receber um paramentro 
+                               a igualdade define o valor 0 ou a criterio*/
+    return n1 + n2
+}
+
+console.log(soma(2, 1))
